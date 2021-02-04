ac29e081d96239b7cc8d3cf1a4fcd9d4f994ac23
Oshan12345 <bsagor99@gmail.com>
2021-02-04 19:31:48 +0600

    integrated weather api

---
+++ api-code.js
@@ -0,0 +1,20 @@
+function getWeatherInfo() {
+  const city = document.getElementById("city-name-input").value;
+  const cityName = document.getElementById("city-name");
+  const weatherCondition = document.getElementById("weather-condition");
+  const temperature = document.getElementById("temperature");
+  const feltTemperature = document.getElementById("felt-temperature");
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=ef53c93ce82e17b8339e5cf153dc16b7`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      cityName.innerText = data.name;
+      weatherCondition.innerText = data.weather[0].main;
+      feltTemperature.innerText = data.main.feels_like;
+      temperature.innerText = data.main.temp;
+    })
+    .catch((err) => console.log(err));
+}
